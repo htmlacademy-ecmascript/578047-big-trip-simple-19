cdfe0a709eda8f86565cb349124dbede56fbc6a2
Artemiy Butin <damnfantasy@list.ru>
2023-01-07 15:26:35 +0700

    module2-task1

--- src/main.js
+++ src/main.js
@@ -1,15 +1,30 @@
 import FilterView from './view/filter-view.js';
 import SortView from './view/sort-view.js';
+import PointsModel from './model/points-model.js';
+import OffersModel from './model/offers-model.js';
+import DestinationsModel from './model/destination-model.js';
 import TripEventsPresenter from './presenter/trip-events-presenter.js';
+
 import {render} from './render.js';
 
 const siteHeaderElement = document.querySelector('.page-header');
-const filterElement = siteHeaderElement.querySelector('.trip-controls__filters');
-const tripEventsElement = document.querySelector('.trip-events');
-const tripEventsPresenter = new TripEventsPresenter({tripEventsContainer: tripEventsElement});
+const siteMainElement = document.querySelector('.page-main');
 
-render(new FilterView(), filterElement);
+const filtersContainerElement = siteHeaderElement.querySelector('.trip-controls__filters');
+const tripEventsElement = siteMainElement.querySelector('.trip-events');
+
+render(new FilterView(), filtersContainerElement);
 render(new SortView(), tripEventsElement);
 
-tripEventsPresenter.init();
+const pointsModel = new PointsModel();
+const offersModel = new OffersModel();
+const destinationsModel = new DestinationsModel();
+
+const routePointsPresenter = new TripEventsPresenter({tripEventsContainer: tripEventsElement,
+  pointsModel,
+  offersModel,
+  destinationsModel
+});
+
+routePointsPresenter.init();
 
